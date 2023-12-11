5ab8b920d9814ab2c0c48324f17e929b6f90d048
luciana.lucio <luciana.lucio@g.globo>
2023-12-11 18:44:39 -0300

    Aula 010 - Parte 03

---
+++ 010-DOM/003-selecionando-elementos/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const p = document.querySelectorAll("p");
+
+console.log(p);
+
+p.forEach((element, index) => {
+    element.style.color = "#FFF";
+    element.style.padding = "20px";
+
+    if (index ===0) {
+        element.style.background = "blue";
+    }
+
+    if (index ===1) {
+        element.style.background = "red";
+    }
+});
