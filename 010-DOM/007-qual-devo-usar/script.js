e591a98b9e4abdc41a98ae258574d4aca242b426
luciana.lucio <luciana.lucio@g.globo>
2023-12-11 19:24:30 -0300

    Aula 010 - Parte 07

---
+++ 010-DOM/007-qual-devo-usar/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+Mas, qual eu devo usar?
+
+Dar preferência:
+- querySelector
+- querySelectorAll => NodeList
+
+Usar menos:
+- getElementById
+- getElementByClassName
+- getElementByTagName
+*/
