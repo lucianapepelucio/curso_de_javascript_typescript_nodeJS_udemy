dd0e2dcf71910196444e908f20b0406c5b17b943
luciana.lucio <luciana.lucio@g.globo>
2023-12-11 18:52:16 -0300

    Aula 010 - Parte 04

---
+++ 010-DOM/004-selecionando-elementos/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const p = document.getElementById("paragrafo"); 
+
+console.log(p);
+
+// Pega o primeiro id criado com determinado nome
+// É errado uma página HTML ter mais de um ID
