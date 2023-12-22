f5656f4e4f9000b07051793003efd11c63904ef2
luciana.lucio <luciana.lucio@g.globo>
2023-12-22 19:33:09 -0300

    Aula 010 - Parte 05

---
+++ 010-DOM/010-encontrando-elementos/005-nextSibling-e-nextElementSibling/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+
+console.log(h1.nextSibling); 
+console.log(h1.nextElementSibling); 
+
+const p = document.querySelector(".paragrafo");
+
+console.log(p.nextSibling); 
+console.log(p.nextElementSibling); 
