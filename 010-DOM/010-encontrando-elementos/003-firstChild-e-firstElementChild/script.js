b58443a8d36211b6f3a05a4180f8a1bfaad8d068
luciana.lucio <luciana.lucio@g.globo>
2023-12-22 19:26:30 -0300

    Aula 010 - Parte 03 e 04

---
+++ 010-DOM/010-encontrando-elementos/003-firstChild-e-firstElementChild/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const container = document.querySelector(".container");
+
+console.log(container.firstChild); 
+console.log(container.firstElementChild); 
+
+const p = document.querySelector(".paragrafo");
+
+console.log(p.firstChild); 
+console.log(p.firstElementChild); 
