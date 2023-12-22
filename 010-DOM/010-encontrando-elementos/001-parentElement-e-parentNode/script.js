612e32e5e498ea9fc34e9b45da1d903e2a77a084
luciana.lucio <luciana.lucio@g.globo>
2023-12-22 18:56:26 -0300

    Aula 010 - Parte 01

---
+++ 010-DOM/010-encontrando-elementos/001-parentElement-e-parentNode/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const strong = document.querySelector("strong");
+
+console.log(strong.parentElement);
+console.log(strong.parentNode);
+
+const p = document.querySelector(".paragrafo");
+
+console.log(p.parentElement);
+console.log(p.parentNode);
