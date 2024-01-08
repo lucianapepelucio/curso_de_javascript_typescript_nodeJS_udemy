c49ca1ba46d9fad977d50e1357751b7142e64f23
luciana.lucio <luciana.lucio@g.globo>
2024-01-08 20:07:42 -0300

    Aula 011 - Parte 07

---
+++ 010-DOM/011-eventos/007-validando-eventos/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const menu = document.querySelector("#menu");
+
+// menu?.addEventListener("click", (event) => {
+//     const { target } = event;
+//     const body = document.querySelector("body");
+
+//     switch (target.getAttribute("class")) {
+//         case "home":
+//             body.style.background = "blue";
+//             break;
+
+//         case "sobre":
+//             body.style.background = "red";
+//             break;
+
+//         case "contato":
+//             body.style.background = "green";
+//             break;
+//     }
+// })
+
+if (menu) {
+    menu.addEventListener("click", (event) => {
+        const { target } = event;
+        const body = document.querySelector("body");
+    
+        switch (target.getAttribute("class")) {
+            case "home":
+                body.style.background = "blue";
+                break;
+    
+            case "sobre":
+                body.style.background = "red";
+                break;
+    
+            case "contato":
+                body.style.background = "green";
+                break;
+        }
+    })
+}
