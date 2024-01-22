2bd53e29e77e2d6afc1066a9797ba8a588239ea7
luciana.lucio <luciana.lucio@g.globo>
2024-01-22 19:05:28 -0300

    Aula 012 - Parte 03

---
+++ 010-DOM/012-formularios/003-manipulando-input/script.js
@@ -0,0 +1,14 @@
+" use strict";
+
+const form = document.forms.namedItem("registration");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const name = form.name.value;
+
+    const newDivElement = document.createElement("div");
+    newDivElement.innerText = name.toUpperCase();
+    form.nextElementSibling.remove();
+    form.after(newDivElement);
+});
