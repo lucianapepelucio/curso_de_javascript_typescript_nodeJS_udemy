812b4f3f39d886c0d8a56942cbf72d9193333131
luciana.lucio <luciana.lucio@g.globo>
2024-02-15 18:50:19 -0300

    Aula 013 - Parte 04

---
+++ 013-javascript-assincrono/004-promise-all/index.js
@@ -0,0 +1,19 @@
+const pedido = (pedido) => {
+    return new Promise((resolve, reject) => {
+        if (pedido === "Pizza") {
+            return reject(`Não temos o seu pedido: [${pedido}]`);
+        }
+
+        setTimeout(() => {
+            resolve(`Chegou o seu pedido [${pedido}]`);
+        }, 5000)
+    })
+}
+
+Promise.all([pedido("Arroz"), pedido("carne"), pedido("Pizza")]).then((resPedidos) => {
+    console.log(resPedidos[0]);
+    console.log(resPedidos[1]);
+})
+.catch((error) => {
+    console.log(error);
+})
